709778ddd8ed2f0b2eb63e8516d0083072c6d0f7
Max Zhu <maxzhu@Coop-MacBook-Pro-1.local>
2020-07-06 09:44:30 -0400

    Add linter

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+   env: {
+     browser: true,
+     es6: true,
+     node: true,
+   },
+   extends: ['prettier', 'plugin:import/errors', 'prettier/react'],
+   globals: {
+     Atomics: 'readonly',
+     SharedArrayBuffer: 'readonly',
+   },
+   parserOptions: {
+     ecmaFeatures: {
+       jsx: true,
+     },
+     ecmaVersion: 11,
+     sourceType: 'module',
+   },
+   plugins: ['react', 'react-hooks', 'prettier'],
+   rules: {
+     'max-len': [
+       'error',
+       {
+         code: 100,
+         comments: 80,
+         ignoreStrings: true,
+         ignoreTemplateLiterals: true,
+       },
+     ],
+     'prettier/prettier': 'error',
+   },
+ };
